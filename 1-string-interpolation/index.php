03d4454bc8c6548a315b8a5b85e62ccf0e526565
Robin <1105080+openscript@users.noreply.github.com>
2021-10-04 08:56:55 +0000

    Add example string interpolation

---
+++ 1-string-interpolation/index.php
@@ -0,0 +1,12 @@
+<?php
+// Eine Variable definieren
+$name = "Hans";
+
+// Zwei Strings zusammenfügen
+echo "Hello " . $name;
+
+// Einen neuen String mit String Interpolation erstellen
+echo "Hello $name";
+
+// String Interpolation funktioniert nur mit doppelten Anführungszeichen
+echo 'Hello $name';
